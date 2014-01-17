4115440d3dc53f85596fd0ae854d688e029d3dc2
Nicholas Anderson <nicholasaa@gmail.com>
2014-01-16 23:24:24 -0800

    Init

---
+++ FaceTheMouse2D/Assets/BaseObjects/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject prefabBullet;
+	public float shootForce;
+	public Transform shootposition;
+	//public GameObject instanceBullet;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey("z"))
+	   	{
+			var instanceBullet = Instantiate (prefabBullet, transform.position, shootposition.rotation) as GameObject;
+			instanceBullet.rigidbody2D.AddForce(shootposition.right * shootForce);
+		}
+	}
+}
---
+++ FaceTheMouse2D/Assets/BaseObjects/Shot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+	//GameObject turret;
+	public Vector3 ScreenPoint;
+	public Vector2 ScreenSize;
+
+	// Use this for initialization
+	void Start () {
+		ScreenSize.x = Screen.width;
+		ScreenSize.y = Screen.height;
+		/*Vector2 force;
+		float speed = 10;
+		turret = GameObject.Find ("turret");
+		transform.rotation = turret.transform.rotation;
+		force = new Vector2 (transform.eulerAngles.x, transform.eulerAngles.y);
+		force.Normalize ();
+		force = new Vector2 (force.x * speed, force.y * speed);
+		rigidbody2D.AddForce (force);*/
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ScreenPoint = Camera.main.WorldToScreenPoint (transform.position);
+		if( (ScreenPoint.x >= Screen.width || ScreenPoint.y<= 0 )||(ScreenPoint.y >= Screen.width || ScreenPoint.y <= 0 ))
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
